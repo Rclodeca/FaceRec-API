1ed21cdb6598ffa04e5907e5a697e0c3c33c2da3
Rclodeca <ryan@theclodes.com>
2018-06-24 16:57:57 -0700

    created server.js

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+app.use(bodyParser.json());
+
+const database = {
+	users: [
+		{
+			id: '123',
+			name: 'John',
+			email: 'john@gmail.com',
+			password: 'cookies',
+			entries: 0,
+			joined: new Date()
+		},
+		{
+			id: '124',
+			name: 'Sally',
+			email: 'sally@gamil.com',
+			password: 'banans',
+			entries: 0,
+			joined: new Date()
+		}
+	]
+}
+
+app.get('/', (req, res) => {
+	res.send(database.users); 
+})
+
+app.post('/signin', (req, res) => {
+	if (req.body.email === database.users[0].email 
+		&& req.body.password === database.users[0].password) {
+		res.json('success');
+	} else {
+		res.status(400).json('error logging in');
+	}
+	res.json('signed in');
+});
+
+app.post('/register', (req, res) => {
+	const {email, name, password} = req.body;
+	database.users.push({
+		id: '125',
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	});
+	res.json(database.users[database.users.length-1]);
+});
+
+app.get('/profile/:id', (req, res) => {
+	const { id } = req.params;
+	let found = false;
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true;
+			return res.json(user);
+		} 
+	});
+	if (!found) {
+		res.status(400).json('user not found');
+	}
+});
+
+
+app.listen(3000, () => {
+	console.log('app is running on port 3000');
+});
