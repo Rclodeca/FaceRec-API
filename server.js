027949d47d74ae39b446a33d47cb50962f03327e
Rclodeca <ryan@theclodes.com>
2018-06-24 19:26:06 -0700

    configured signin

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
 	users: [
@@ -11,7 +14,7 @@ const database = {
 			id: '123',
 			name: 'John',
 			email: 'john@gmail.com',
-			password: 'cookies',
+			password: "cookies",
 			entries: 0,
 			joined: new Date()
 		},
@@ -19,9 +22,16 @@ const database = {
 			id: '124',
 			name: 'Sally',
 			email: 'sally@gamil.com',
-			password: 'banans',
+			password: "bananas",
 			entries: 0,
 			joined: new Date()
+		}
+	],
+	login: [
+		{
+			id: '987',
+			hash: '',
+			email: 'john@gamil.com'
 		}
 	]
 }
@@ -42,11 +52,16 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
 	const {email, name, password} = req.body;
+
+	bcrypt.hash(password, null, null, function(err, hash) {
+    	console.log(hash);
+	});
+
 	database.users.push({
 		id: '125',
 		name: name,
 		email: email,
-		password: password,
+		password: hash,
 		entries: 0,
 		joined: new Date()
 	});
@@ -81,6 +96,20 @@ app.put('/image', (req, res) => {
 		res.status(400).json('user not found');
 	}
 });
+
+
+/*bcrypt.hash("bacon", null, null, function(err, hash) {
+    // Store hash in your password DB.
+});
+
+// Load hash from your password DB.
+bcrypt.compare("bacon", hash, function(err, res) {
+    // res == true
+});
+bcrypt.compare("veggies", hash, function(err, res) {
+    // res = false
+});*/
+
 
 app.listen(3000, () => {
 	console.log('app is running on port 3000');
