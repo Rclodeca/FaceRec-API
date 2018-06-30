4ecdc9f50d5f627ee58a9cc9ee11ce008441cad3
Rclodeca <ryan@theclodes.com>
2018-06-29 19:21:09 -0700

    create register.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const cors = require('cors');
 const knex = require('knex');
 const DBkey = require('./DBkey.json');
 
+const register = require('./controllers/register.js'); 
+
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -47,35 +50,8 @@ app.post('/signin', (req, res) => {
 		.catch(err => res.status(400).json('Wrong email or password'));
 });
 
-app.post('/register', (req, res) => {
-	const {email, name, password} = req.body;
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt); });
 
-	const hash = bcrypt.hashSync(password);
-
-	db.transaction(trx => {
-		trx.insert({
-			hash: hash,
-			email: email
-		})
-		.into('login')
-		.returning('email')
-		.then(loginEmail => {
-			return trx('users')
-			.returning('*')
-			.insert({
-				email: loginEmail[0],
-				name: name,
-				joined: new Date()
-			})
-			.then(user => {
-				res.json(user[0]);
-			})
-		})
-		.then(trx.commit)
-		.catch(trx.rollback)
-	})
-	.catch(err => res.status(400).json('Unable to register user'));
-});
 
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
